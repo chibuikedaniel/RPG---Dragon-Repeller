d0006805e77bac7d478e86de589fd4e4c581e218
dracuzi <danielchibuike77@gmail.com>
2024-08-20 23:52:38 +0100

    added logic to the buy health function

--- script.js
+++ script.js
@@ -28,6 +28,12 @@ const locations = [
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
     }
 ];
 
@@ -55,7 +61,7 @@ function goStore() {
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon() {
@@ -63,8 +69,21 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+    goldText.innerText = gold -= 10;
+    healthText.innerText = health += 10;
+    // or
+    /*gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;*/
 }
 function buyWeapon() {
+
+}
+
+function fightSlime() {
+
+}
+function fightBeast() {
 
 }
