ecf07d0d14c1694bbca99e16309796cb6ba6ef40
dracuzi <danielchibuike77@gmail.com>
2024-08-21 01:25:45 +0100

    added logic to the buy weapon function

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+    {
+        name: "stick",
+        power: 5
+    },
+    {
+        name: "dagger",
+        power: 30
+    },
+    {
+        name: "claw hammer",
+        power: 50
+    },
+    {
+        name: "sword",
+        power: 100
+    }
+];
 const locations = [
     {
         name: "town square",
@@ -69,16 +87,33 @@ function fightDragon() {
 }
 
 function buyHealth() {
-    goldText.innerText = gold -= 10;
-    healthText.innerText = health += 10;
-    // or
-    /*gold -= 10;
-    health += 10;
-    goldText.innerText = gold;
-    healthText.innerText = health;*/
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health."
+    }
 }
 function buyWeapon() {
-
+    if (currentWeaponIndex < weapons.length - 1) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeaponIndex++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeaponIndex].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory.join(", ");
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon."
+        }
+    } else {
+        text.innerText = "You already have the best weapons already!"; // Message if the player has all the weapons
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
 }
 
 function fightSlime() {
