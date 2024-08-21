d0d448b8d47a84af5d25132d52b205832edb0542
dracuzi <danielchibuike77@gmail.com>
2024-08-21 22:54:56 +0100

    added monsters and some fighting functions

--- script.js
+++ script.js
@@ -34,6 +34,23 @@ const weapons = [
         power: 100
     }
 ];
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15,
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60,
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300,
+    }
+]
 const locations = [
     {
         name: "town square",
@@ -52,6 +69,12 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
+    },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
     }
 ];
 
@@ -80,10 +103,6 @@ function goStore() {
 
 function goCave() {
     update(locations[2]);
-}
-
-function fightDragon() {
-    console.log("Fighting dragon.");
 }
 
 function buyHealth() {
@@ -129,8 +148,27 @@ function sellWeapon() {
 }
 
 function fightSlime() {
-
+    fighting = 0;
+    goFight();
 }
 function fightBeast() {
+    fighting = 1;
+    goFight();
+}
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+}
+function attack() {
+
+}
+function dodge() {
 
 }
